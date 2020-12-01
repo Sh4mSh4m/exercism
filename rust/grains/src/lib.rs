7fd5228c5cc0c6d2804181d35c0e9df708efb2af
Hieu TRUONG <htruong@Hieus-MacBook-Pro.local>
2020-12-01 19:06:12 +0100

    grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,25 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    //unimplemented!("grains of rice on square {}", s);
+    if 1 <= s && s <= 64 {
+        2u64.pow(s-1)
+    } else {
+        panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).map(|x| square(x)).sum::<u64>()
 }
+
+// see solution with binary operator leftshift on integers
+//pub fn square(s: u32) -> u64 {
+//	if s < 1 || s > 64 {
+//		panic!("Square must be between 1 and 64")
+//	} else {
+//		1u64 << (s - 1)
+//	}
+//}
+//
+//pub fn total() -> u64 {
+//	u64::max_value()
+//}
